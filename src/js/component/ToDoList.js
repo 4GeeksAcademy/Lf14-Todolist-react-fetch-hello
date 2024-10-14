42fff74004e1f731d0f6e7be4c6b36e849f2d114
lf14 <54648106+lf14@users.noreply.github.com>
2024-10-14 14:58:38 +0000

    [Completado entrega]
    ToDoList.js- se agrega condición también al efecto para que no se muestre si no hay tareas.
    Index.js

--- src/js/component/ToDoList.js
+++ src/js/component/ToDoList.js
@@ -11,7 +11,7 @@ const ToDoList = () => {
       setToDo("");
     }
   };
-  
+
   return (
     <div className="text-center container my-5 ">
       <h1 className="text-muted "><em>ToDoList</em></h1>
@@ -22,36 +22,37 @@ const ToDoList = () => {
         onChange={(e) => setToDo(e.target.value)}
         onKeyDown={handleKeyDown}
       />
-      
+
       <ol className="todo-list list-group text-start">
         {toDoList.map((task, index) => (
           <li className="list-group-item todo-item" key={index}>
-          {task}
-          <button
-            type="button"
-            className="btn-close"
-            aria-label="Close"
-            onClick={() => {
-              setToDoList((prevListItem) => prevListItem.filter((_, i) => i !== index));
-            }}
-          ></button>
-        </li>
+            {task}
+            <button
+              type="button"
+              className="btn-close"
+              aria-label="Close"
+              onClick={() => {
+                setToDoList((prevListItem) => prevListItem.filter((_, i) => i !== index));
+              }}
+            ></button>
+          </li>
         ))}
         {toDoList.length > 0 && (
           <li className="list-group-item text-muted">
             Total Tasks: {toDoList.length}
           </li>
         )}
-        <div clasName="leaf-container">
-          <div className="leaf-1 p-1 shadow">
-          </div>
-          <div className="leaf-2 p-1 shadow">
-          </div>
-          <div className="leaf-3 p-1 shadow">
-          </div>
-          </div>
+        {toDoList.length > 0 && (
+          <div clasName="leaf-container">
+            <div className="leaf-1 p-1 shadow">
+            </div>
+            <div className="leaf-2 p-1 shadow">
+            </div>
+            <div className="leaf-3 p-1 shadow">
+            </div>
+          </div>)}
       </ol>
-  
+
     </div>
   );
 };
